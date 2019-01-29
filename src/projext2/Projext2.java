d0f358cac0f2c2f651b39fb081c9f477e0a5fea1
ahmad aref <ahmad691998@gmail.com>
2019-01-29 14:06:45 +0200

    make some change for check

--- src/projext2/Projext2.java
+++ src/projext2/Projext2.java
@@ -1,21 +1,11 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package projext2;
 
-/**
- *
- * @author jit
- */
 public class Projext2 {
 
-    /**
-     * @param args the command line arguments
-     */
+   
     public static void main(String[] args) {
-        // TODO code application logic here
+       
     }
     
 }
