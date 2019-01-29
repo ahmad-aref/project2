3d70bf647cfa151ff758d165d76c4e9fe2d68704
ahmad aref <ahmad691998@gmail.com>
2019-01-29 20:28:43 +0200

    add method to sum to array

--- src/projext2/Projext2.java
+++ src/projext2/Projext2.java
@@ -15,6 +15,17 @@ public class Projext2 {
         Scanner input = new Scanner(file);
     }
     
+        public static int[][] SumAB(int[][] A, int[][] B) {
+        int[][] SumAB = new int[A.length][A[0].length];
+        for (int i = 0; i < A.length; i++) {
+            for (int j = 0; j < A[0].length; j++) {
+                SumAB[i][j] = A[i][j] + B[i][j];
+            } 
+        }
+
+        return SumAB;
+    }
+        
        public static int[][] Transpose(int[][] M) {
         int[][] transposeM = new int[M[0].length][M.length];
         for (int i = 0; i < transposeM.length ; i++) {
