fdcbfbeed7fb789913bbbe4e4be78a7b73f2152a
alaahamouda <alaahamouda12017@gmail.com>
2019-01-29 21:06:14 +0200

    write some comment in the code

--- src/projext2/Projext2.java
+++ src/projext2/Projext2.java
@@ -14,7 +14,8 @@ public class Projext2 {
         PrintWriter file_output = new PrintWriter("output.txt");
         Scanner input = new Scanner(file);
     }
-    
+        
+        // method use to sum two Arrays we pass the array A and B then return array of sum namde SumAB
         public static int[][] SumAB(int[][] A, int[][] B) {
         int[][] SumAB = new int[A.length][A[0].length];
         for (int i = 0; i < A.length; i++) {
@@ -25,6 +26,8 @@ public class Projext2 {
 
         return SumAB;
     }
+        
+         // method use to subtract two Arrays we pass the array A and B then return array of subtract SUBTAB 
          public static int[][] SUBTAB(int[][] A, int[][] B) {
         int[][] SUBTAB = new int[A.length][A[0].length];
         for (int i = 0; i < A.length; i++) {
@@ -35,7 +38,9 @@ public class Projext2 {
 
         return SUBTAB;
     }
-        
+         
+         
+         // method use to Transpose an Array >>  we pass the array M then return array result transposeM
        public static int[][] Transpose(int[][] M) {
         int[][] transposeM = new int[M[0].length][M.length];
         for (int i = 0; i < transposeM.length ; i++) {
