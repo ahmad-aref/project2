9621c72f948237f8a6c3f0f36353d91aa0e1770c
ahmad aref <ahmad691998@gmail.com>
2019-01-29 14:17:11 +0200

    make output.txt file

--- src/projext2/Projext2.java
+++ src/projext2/Projext2.java
@@ -1,11 +1,18 @@
 
 package projext2;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.Scanner;
+
 public class Projext2 {
 
    
-    public static void main(String[] args) {
-       
+    public static void main(String[] args) throws FileNotFoundException {
+        File file = new File("input.txt");
+        PrintWriter file_output = new PrintWriter("output.txt");
+        Scanner input = new Scanner(file);
     }
     
 }
