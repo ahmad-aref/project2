bb5c966302b3a07af56907d2a852136a5fc10b90
ahmad aref <ahmad691998@gmail.com>
2019-01-30 18:48:47 +0200

    format project

--- src/projext2/Projext2.java
+++ src/projext2/Projext2.java
@@ -1,4 +1,3 @@
-
 package projext2;
 
 import java.io.File;
@@ -8,20 +7,20 @@ import java.util.Scanner;
 
 public class Projext2 {
 
-   
     public static void main(String[] args) throws FileNotFoundException {
         File file = new File("input.txt");
         PrintWriter file_output = new PrintWriter("output.txt");
         Scanner input = new Scanner(file);
-        
+
     }
-        public static int[][] MUL(int[][] A , int[][] B){
+
+    public static int[][] MUL(int[][] A, int[][] B) {
         int[][] MULAB = new int[A.length][B[0].length];
         int sum = 0;
-        for(int i = 0 ; i < A.length ; i++){
+        for (int i = 0; i < A.length; i++) {
             for (int j = 0; j < B[0].length; j++) {
-                for (int n = 0 ; n < B.length ; n++){
-                   sum += A[i][n] * B[n][j];  
+                for (int n = 0; n < B.length; n++) {
+                    sum += A[i][n] * B[n][j];
                 }
                 MULAB[i][j] = sum;
                 sum = 0;
@@ -29,45 +28,44 @@ public class Projext2 {
         }
         return MULAB;
     }
-        
-        // method use to sum two Arrays we pass the array A and B then return array of sum namde SumAB
-        public static int[][] SumAB(int[][] A, int[][] B) {
+
+    // method use to sum two Arrays we pass the array A and B then return array of sum namde SumAB
+    public static int[][] SumAB(int[][] A, int[][] B) {
         int[][] SumAB = new int[A.length][A[0].length];
         for (int i = 0; i < A.length; i++) {
             for (int j = 0; j < A[0].length; j++) {
                 SumAB[i][j] = A[i][j] + B[i][j];
-            } 
+            }
         }
 
         return SumAB;
     }
-        
-         // method use to subtract two Arrays we pass the array A and B then return array of subtract SUBTAB 
-         public static int[][] SUBTAB(int[][] A, int[][] B) {
+
+    // method use to subtract two Arrays we pass the array A and B then return array of subtract SUBTAB 
+    public static int[][] SUBTAB(int[][] A, int[][] B) {
         int[][] SUBTAB = new int[A.length][A[0].length];
         for (int i = 0; i < A.length; i++) {
             for (int j = 0; j < A[0].length; j++) {
                 SUBTAB[i][j] = A[i][j] - B[i][j];
-            } 
+            }
         }
 
         return SUBTAB;
     }
-         
-         
-         // method use to Transpose an Array >>  we pass the array M then return array result transposeM
-       public static int[][] Transpose(int[][] M) {
+
+    // method use to Transpose an Array >>  we pass the array M then return array result transposeM
+    public static int[][] Transpose(int[][] M) {
         int[][] transposeM = new int[M[0].length][M.length];
-        for (int i = 0; i < transposeM.length ; i++) {
-            for (int j = 0; j < transposeM[0].length ; j++) {
+        for (int i = 0; i < transposeM.length; i++) {
+            for (int j = 0; j < transposeM[0].length; j++) {
                 transposeM[i][j] = M[j][i];
             }
         }
-        
+
         return transposeM;
     }
-       
-           public static int DET(int[][] A) {
+
+    public static int DET(int[][] A) {
         int test[][];
         int Result = 0;
 
@@ -92,9 +90,9 @@ public class Projext2 {
                 }
             }
 
-            Result += A[0][i] * Math.pow (-1, i) * DET(test);
+            Result += A[0][i] * Math.pow(-1, i) * DET(test);
         }
         return Result;
     }
-    
+
 }
